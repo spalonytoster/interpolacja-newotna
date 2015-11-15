1469a289ef315fc97c9d5dc317a4ecd360d57c67
Maciej Posłuszny <xmachiei@gmail.com>
2015-11-15 20:33:09 +0100

    usunięto tymczasowe pliki pdf i r

--- interpolacja-skrypt.r
+++
@@ -1,2 +0,0 @@
-curve(1.0*x^2-1.0*x, 0, 5, add=TRUE, col="green") ; abline(h=0,v=0,lty=3)
-curve(-0.7*x^4+8.399999999999999*x^3-33.3*x^2+53.599999999999994*x-28.0, 0, 5, add=TRUE, col="red") ; abline(h=0,v=0,lty=3)
